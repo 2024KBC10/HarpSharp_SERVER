9bb4dccb6eb0eeea320b12755fd555be7233b099
mng990 <ryoo0504@gmail.com>
2024-07-26 15:27:07 +0900

    fix: 루트 디렉토리 재설정

--- auth/scripts/deploy-auth.sh
+++ auth/scripts/deploy-auth.sh
@@ -6,9 +6,10 @@ docker rm  -f $(docker ps -a -q)
 cd /home/ubuntu/deploy-auth/zip/
 
 docker build -t auth ./          # Docker Image 생성
+
 aws s3 cp s3://haaaarp/HarpSharp_API_Auth.json ./
 
 docker run --name db --network=harpsharp -e MYSQL_ROOT_PASSWORD=wlghks24461! -d -p 3306:3306 -v mysql-data:/var/lib/mysql mysql:latest
 docker run --name redis --network=harpsharp -d -p 6379:6379 -v redis-data:/data redis
-docker run -d -p 80:8080 --name swagger -e SWAGGER_JSON=/tmp/HarpSharp_API_Auth.json -v ./:/tmp swaggerapi/swagger-ui
-docker run --name auth --network=harpsharp -d -p 4242:4242 auth  # Docker Container 생성
+docker run -d -p 80:8000 --name swagger -e SWAGGER_JSON=/tmp/HarpSharp_API_Auth.json -v ./:/tmp swaggerapi/swagger-ui
+docker run --name auth --network=harpsharp -d -p 8080:8080 auth  # Docker Container 생성
