09d9dd51743956979599c9eb22fcc2640048e47d
mng990 <ryoo0504@gmail.com>
2024-07-26 09:03:52 +0900

    test: 배포 테스트

--- auth/scripts/deploy-auth.sh
+++ auth/scripts/deploy-auth.sh
@@ -2,7 +2,9 @@
 
 mkdir -p /home/ubuntu/deploy-auth/zip/
 
-docker rm -f $(docker ps -a -q)
+docker rm  -f $(docker ps -a -q)
+docker rmi -f $(docker images -q)
+
 sudo service docker restart
 
 cd /home/ubuntu/deploy-auth/zip/
@@ -10,7 +12,6 @@ cd /home/ubuntu/deploy-auth/zip/
 docker build -t auth ./          # Docker Image 생성
 aws s3 cp s3://haaaarp/HarpSharp_API_Auth.json ./
 
-docker network create harpsharp
 docker run --name db --network=harpsharp -e MYSQL_ROOT_PASSWORD=wlghks24461! -d -p 3306:3306 -v mysql-data:/var/lib/mysql mysql:latest
 docker run --name redis --network=harpsharp -d -p 6379:6379 -v redis-data:/data redis
 docker run -d -p 80:8080 --name swagger -e SWAGGER_JSON=/tmp/HarpSharp_API_Auth.json -v ./:/tmp swaggerapi/swagger-ui
