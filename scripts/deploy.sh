c1af064efd49ab62a37b8b5838e1bbc3d2d03923
mng990 <ryoo0504@gmail.com>
2024-07-31 07:29:32 +0900

    fix: 도커 이미지 삭제 로직 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,23 +9,48 @@ else
     echo "harpsharp 네트워크가 이미 존재합니다."
 fi
 
-cd /home/ubuntu/deploy
-
 cd /home/ubuntu/deploy/db/
 
 docker-compose up -d
 
-docker stop auth | rm auth | rmi auth
-docker stop swagger-auth | rm swagger-auth | rmi swagger-auth
 
 cd /home/ubuntu/deploy/auth/
 
+if [ $(docker ps -a -q -f name=auth) ]; then
+    docker stop auth && docker rm auth
+fi
+if [ $(docker images -q auth) ]; then
+    docker rmi auth
+fi
+
+# 'swagger-board' 컨테이너와 이미지 삭제
+if [ $(docker ps -a -q -f name=swagger-auth) ]; then
+    docker stop swagger-auth && docker rm swagger-auth
+fi
+if [ $(docker images -q swagger-auth) ]; then
+    docker rmi swagger-auth
+fi
+
 docker-compose up --build -d
 
-docker stop board | rm board | rmi board
-docker stop swagger-board | rm swagger-board | rmi swagger-board
 
 cd /home/ubuntu/deploy/board/
+
+# 'board' 컨테이너와 이미지 삭제
+if [ $(docker ps -a -q -f name=board) ]; then
+    docker stop board && docker rm board
+fi
+if [ $(docker images -q board) ]; then
+    docker rmi board
+fi
+
+# 'swagger-board' 컨테이너와 이미지 삭제
+if [ $(docker ps -a -q -f name=swagger-board) ]; then
+    docker stop swagger-board && docker rm swagger-board
+fi
+if [ $(docker images -q swagger-board) ]; then
+    docker rmi swagger-board
+fi
 
 docker-compose up --build -d
 
