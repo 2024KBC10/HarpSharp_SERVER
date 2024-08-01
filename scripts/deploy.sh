0390e4d6b6ea8b6d27af0d1da06512b61c05918e
mng990 <ryoo0504@gmail.com>
2024-08-02 02:42:44 +0900

    fix: nginx conf 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -43,22 +43,10 @@ serviceDown(){
       docker rmi -f $IMAGE_ID
       fi
   fi
-
-  if [ $(docker ps -a -q -f name=swagger-$CONTAINER_NAME) ]; then
-        echo "컨테이너 $CONTAINER_NAME 종료 및 삭제 중..."
-
-        IMAGE_ID=$(docker images -q $CONTAINER_NAME)
-
-        if [ "$IMAGE_ID" ]; then
-            echo "이미지 $CONTAINER_NAME 삭제 중..."
-
-        docker rmi -f $IMAGE_ID
-        fi
-    fi
 }
 
 restartNginx(){
-  docker exec nginx nginx -s reload
+  docker exec -it nginx service nginx reload
 }
 
 cleanUpImages(){
