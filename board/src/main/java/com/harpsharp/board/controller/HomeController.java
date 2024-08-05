97532917f3183f76998909718f9da041119c0c4f
mng990 <ryoo0504@gmail.com>
2024-08-06 02:28:48 +0900

    fix: /board proxy_pass 추가 + 리다이렉션 로직 롤백

--- board/src/main/java/com/harpsharp/board/controller/HomeController.java
+++ board/src/main/java/com/harpsharp/board/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.harpsharp.board.controller;
 
+import com.harpsharp.auth.utils.BaseResponse;
+import com.harpsharp.infra_rds.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/board")
-    public String home() {
-        return "index";
+    public ResponseEntity<ApiResponse> home() {
+        return BaseResponse.withCode("WELCOME_TO_BOARD", "This is Homepage Of Board!", HttpStatus.OK);
     }
 }
