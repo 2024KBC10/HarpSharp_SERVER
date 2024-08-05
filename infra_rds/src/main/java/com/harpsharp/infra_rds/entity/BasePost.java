53fa911dcfd214de574d940ad1d45dc095d676f8
Song <rmsdud1626@naver.com>
2024-08-05 22:12:42 +0900

    chore: created_at, updated_at 자동화

--- infra_rds/src/main/java/com/harpsharp/infra_rds/entity/BasePost.java
+++ infra_rds/src/main/java/com/harpsharp/infra_rds/entity/BasePost.java
@@ -7,24 +7,26 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter(AccessLevel.PROTECTED)
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // Add this line
 public abstract class BasePost {
     @NotNull
     @Column(name = "content")
     protected String content;
 
-    @ManyToOne (fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     protected User user;
 
     @NotNull
     @CreatedDate
-    @Column(name = "created_at")
+    @Column(name = "created_at", updatable = false)
     protected LocalDateTime createdAt;
 
     @NotNull
