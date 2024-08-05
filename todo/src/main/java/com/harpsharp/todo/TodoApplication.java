60edc45813a5a696132fd8fc17acf21c54861ee4
Song <rmsdud1626@naver.com>
2024-08-05 19:57:40 +0900

    chore: todo scanBasePacakges에 todo 패키지 추가

--- todo/src/main/java/com/harpsharp/todo/TodoApplication.java
+++ todo/src/main/java/com/harpsharp/todo/TodoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.harpsharp.auth", "com.harpsharp.infra_rds"})
+@SpringBootApplication(scanBasePackages = {"com.harpsharp.auth", "com.harpsharp.infra_rds", "com.harpsharp.todo"})
 public class TodoApplication {
 
 	public static void main(String[] args) {
