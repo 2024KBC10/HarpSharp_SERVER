8e206613af3852369c1e0ec8ad6aaa0a33b23554
mng990 <ryoo0504@gmail.com>
2024-08-07 07:01:00 +0900

    add: nginx.conf 프록시 타임아웃을 최대 10분으로 확장

--- auth/src/main/java/com/harpsharp/auth/controller/VerificationController.java
+++ auth/src/main/java/com/harpsharp/auth/controller/VerificationController.java
@@ -18,25 +18,25 @@ public class VerificationController {
     @GetMapping("/verify/posts")
     public ResponseEntity<?> verificationBoard(@RequestHeader("Authorization") String authorization,
                                                          @RequestBody RequestPostDTO requestBody) {
-//        // Authorization 헤더에서 JWT 추출
-//        if (authorization == null || !authorization.startsWith("Bearer ")) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorization header is missing or invalid");
-//        }
-//
-//        String accessToken = authorization.substring("Bearer ".length());
-//
-//        // JWT 클레임과 요청 본문에서 username 추출
-//        String jwtUsername = jwtUtil.getUsername(accessToken);
-//        String requestUsername = requestBody.username();
-//
-//        if (requestUsername == null) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is missing in request body");
-//        }
-//
-//        // username 비교
-//        if (!jwtUsername.equals(requestUsername)) {
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Username does not match");
-//        }
+        // Authorization 헤더에서 JWT 추출
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorization header is missing or invalid");
+        }
+
+        String accessToken = authorization.substring("Bearer ".length());
+
+        // JWT 클레임과 요청 본문에서 username 추출
+        String jwtUsername = jwtUtil.getUsername(accessToken);
+        String requestUsername = requestBody.username();
+
+        if (requestUsername == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is missing in request body");
+        }
+
+        // username 비교
+        if (!jwtUsername.equals(requestUsername)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Username does not match");
+        }
 
         return ResponseEntity.ok("User verified successfully");
     }
