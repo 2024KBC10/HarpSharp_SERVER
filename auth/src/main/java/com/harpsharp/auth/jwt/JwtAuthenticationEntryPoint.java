216e733318e225550297a36d1cd6661235148b8e
mng990 <ryoo0504@gmail.com>
2024-08-08 23:56:13 +0900

    test: login filterchain internal exception을 잡아 정상적인 response 내려주기

--- auth/src/main/java/com/harpsharp/auth/jwt/JwtAuthenticationEntryPoint.java
+++ auth/src/main/java/com/harpsharp/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,34 +1,32 @@
 package com.harpsharp.auth.jwt;
 
 import io.jsonwebtoken.security.SignatureException;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
-@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final HandlerExceptionResolver resolver;
-
-    public JwtAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
-        this.resolver = resolver;
-    }
-
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
 
-        log.info("JwtAuthenticationEntryPoint 실행");
-
-        // HandlerExceptionResolver에게 예외 처리를 위임
-        resolver.resolveException(request, response, null, e);
+        // SignatureException이 원인인 경우 처리
+        Throwable cause = authException.getCause();
+        if (cause instanceof SignatureException) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 코드 반환
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Invalid JWT signature\"}");
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 일반적인 인증 오류
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"Unauthorized\"}");
+        }
     }
 }
