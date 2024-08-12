44d38476d214926247d5aaa00426b7252cc09cd8
mng990 <ryoo0504@gmail.com>
2024-08-12 11:36:24 +0900

    fix: DB를 기존 네트워크와 분리, api 엔드포인트 서버와만 통신하도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-runDB(){
-  MYSQL_CONTAINER="mysql"
-  REDIS_CONTAINER="redis"
-
-  # MySQL 컨테이너가 실행 중인지 확인
-  MYSQL_RUNNING=$(docker ps | grep -q $MYSQL_CONTAINER; echo $?)
-
-  # Redis 컨테이너가 실행 중인지 확인
-  REDIS_RUNNING=$(docker ps | grep -q $REDIS_CONTAINER; echo $?)
-
-  # MySQL 또는 Redis 컨테이너가 실행 중이지 않으면 docker-compose up 실행
-  if [ $MYSQL_RUNNING -ne 0 ] || [ $REDIS_RUNNING -ne 0 ]
-  then
-    echo "MySQL 또는 Redis 컨테이너가 실행 중이지 않습니다. docker-compose up을 실행합니다."
-    cd /home/ubuntu/deploy/db/
-    docker-compose up -d
-  else
-    echo "MySQL 및 Redis 컨테이너가 이미 실행 중입니다."
-  fi
-}
 runNetwork(){
   # 네트워크가 존재하는지 확인
   if [ -z "$(docker network ls | grep harpsharp)" ]
@@ -29,6 +9,14 @@ runNetwork(){
   else
       echo "harpsharp 네트워크가 이미 존재합니다."
   fi
+
+  if [ -z "$(docker network ls | grep datasource)" ]
+    then
+        echo "harpsharp 네트워크를 생성합니다."
+        docker network create datasource
+    else
+        echo "harpsharp 네트워크가 이미 존재합니다."
+    fi
 }
 
 serviceDown(){
@@ -63,7 +51,9 @@ cleanUpImages(){
 
 
 runNetwork
-runDB
+
+cd /home/ubuntu/deploy/db/
+docker-compose up -d
 
 cd /home/ubuntu/deploy/auth
 serviceDown auth
