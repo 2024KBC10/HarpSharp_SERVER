ebc941f70b5609a1d56d4e8e812e53af7e9f9744
mng990 <ryoo0504@gmail.com>
2024-08-21 08:54:33 +0900

    test: logout 테스트

--- auth/src/main/java/com/harpsharp/auth/config/CORSConfig.java
+++ auth/src/main/java/com/harpsharp/auth/config/CORSConfig.java
@@ -12,7 +12,7 @@ public class CORSConfig {
     @Bean
     public CorsConfiguration corsConfiguration() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
