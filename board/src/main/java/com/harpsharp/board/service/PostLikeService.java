35b378770827e7cef8c6f15776c9d31cb58b2b9c
mng990 <ryoo0504@gmail.com>
2024-08-31 17:56:47 +0900

    fix: 좋아요 엔티티가 기록되지 않던 버그 수정

--- board/src/main/java/com/harpsharp/board/service/PostLikeService.java
+++ board/src/main/java/com/harpsharp/board/service/PostLikeService.java
@@ -2,10 +2,14 @@ package com.harpsharp.board.service;
 
 import com.harpsharp.infra_rds.dto.board.like.RequestPostLikeDTO;
 import com.harpsharp.infra_rds.dto.board.like.ResponsePostLikeDTO;
+import com.harpsharp.infra_rds.entity.board.Post;
 import com.harpsharp.infra_rds.entity.board.PostLike;
+import com.harpsharp.infra_rds.entity.user.User;
 import com.harpsharp.infra_rds.mapper.CommentLikeMapper;
 import com.harpsharp.infra_rds.repository.CommentLikeRepository;
 import com.harpsharp.infra_rds.repository.PostLikeRepository;
+import com.harpsharp.infra_rds.repository.PostRepository;
+import com.harpsharp.infra_rds.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,12 +22,24 @@ import java.util.Optional;
 public class PostLikeService {
     private final PostLikeRepository postLikeRepository;
     private final PostService postService;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     public ResponsePostLikeDTO triggerLike(RequestPostLikeDTO requestPostLikeDTO){
         Optional<PostLike> postLike = postLikeRepository.findByUsernameAndPostId(requestPostLikeDTO.username(), requestPostLikeDTO.postId());
 
         if(postLike.isEmpty()){
             Long likeCount = postService.likePost(requestPostLikeDTO);
+            User user = userRepository.findByUsername(requestPostLikeDTO.username()).get();
+            Post post = postRepository.findById(requestPostLikeDTO.postId()).get();
+
+            PostLike newPostLike = PostLike
+                    .builder()
+                    .user(user)
+                    .post(post)
+                    .build();
+
+            postLikeRepository.save(newPostLike);
             return new ResponsePostLikeDTO(requestPostLikeDTO.username(), requestPostLikeDTO.postId(), likeCount);
         }
 
